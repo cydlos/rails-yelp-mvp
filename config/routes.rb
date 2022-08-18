392404f451005f687645271b9cd88b7ff332a7de
cyd losekann <cydnel@gmail.com>
2022-08-17 23:53:20 -0300

    backend done, front missing

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-  get 'reviews/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
   resources :restaurants, except: %i[destroy edit update] do
-    resources :reviews, only: %i[new create]
+    resources :reviews, only: %i[create]
   end
-  root to: 'restaurants#index'
+  root 'restaurants#index'
 end
